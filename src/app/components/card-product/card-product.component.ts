e44ff8b654c870ec35b4f1379a9efbc47538969f
SantiCoVe <cottevelasquez@gmail.com>
2024-02-23 18:12:35 -0500

    prueba

---
+++ src/app/components/card-product/card-product.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-product',
+  templateUrl: './card-product.component.html',
+  styleUrl: './card-product.component.css'
+})
+export class CardProductComponent implements OnInit{
+
+cantidad: number = 0;
+
+//Inicio objeto de producto.
+  product = {
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  }
+//fin objeto de producto.
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+	addProduct() {
+    this.cantidad++;
+  }
+
+  removeProduct() {
+    this.cantidad--;
+  }
+}
